2e26c6afe5ac7ff8f421a0a6dd1b85aca6e10735
sinadn <sina.chat1398@gmail.com>
2021-01-10 17:12:24 +0330

    publish7

--- app/src/main/java/com/example/lappeapp/MainActivity.kt
+++ app/src/main/java/com/example/lappeapp/MainActivity.kt
@@ -13,9 +13,38 @@ import com.example.lappeview.centrallappe.NotifyingScrollView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var lape: LappeView
+    private lateinit var scrollView: NotifyingScrollView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        lape = findViewById(R.id.lappe)
+        scrollView = findViewById(R.id.scroll)
+
+
+
+
+
+        val list: MutableList<String> = arrayListOf()
+        list.add("home")
+        list.add("service")
+        list.add("contact us")
+        list.add("fAQ")
+
+
+        lape.setTitleText(list, R.color.white);
+        lape.setFont(ResourcesCompat.getFont(this, R.font.iran_yekan_bold))
+        lape.setMainColor("#70FF0000")
+        lape.setBgTextColor("#ff0000")
+        lape.setScrollView(scrollView)
+        lape.onClick(ItemOnClick {
+            if (lape.alpha != 0.0f)
+                Toast.makeText(this, "----> " + it, Toast.LENGTH_SHORT).show()
+        })
 
 
 
